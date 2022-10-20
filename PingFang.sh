be47d4acea56db9a396c033c3d9d46caf358907a
Jimmy Cheung <ctk@live.hk>
2022-10-20 10:56:21 +0800

    init upload
    init upload

---
+++ PingFang.sh
@@ -0,0 +1,92 @@
+# PingFangSC
+ttx -t cmap PingFangSC-Light.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangSC-Light.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangSC-Light.ttx
+ttx -b -m PingFangSC-Light.otf PingFangSC-Light.ttx
+
+ttx -t cmap PingFangSC-Medium.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangSC-Medium.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangSC-Medium.ttx
+ttx -b -m PingFangSC-Medium.otf PingFangSC-Medium.ttx
+
+ttx -t cmap PingFangSC-Regular.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangSC-Regular.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangSC-Regular.ttx
+ttx -b -m PingFangSC-Regular.otf PingFangSC-Regular.ttx
+
+ttx -t cmap PingFangSC-Semibold.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangSC-Semibold.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangSC-Semibold.ttx
+ttx -b -m PingFangSC-Semibold.otf PingFangSC-Semibold.ttx
+
+ttx -t cmap PingFangSC-Thin.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangSC-Thin.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangSC-Thin.ttx
+ttx -b -m PingFangSC-Thin.otf PingFangSC-Thin.ttx
+
+ttx -t cmap PingFangSC-Ultralight.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangSC-Ultralight.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangSC-Ultralight.ttx
+ttx -b -m PingFangSC-Ultralight.otf PingFangSC-Ultralight.ttx
+
+# PingFangTC
+ttx -t cmap PingFangTC-Light.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangTC-Light.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangTC-Light.ttx
+ttx -b -m PingFangTC-Light.otf PingFangTC-Light.ttx
+
+ttx -t cmap PingFangTC-Medium.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangTC-Medium.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangTC-Medium.ttx
+ttx -b -m PingFangTC-Medium.otf PingFangTC-Medium.ttx
+
+ttx -t cmap PingFangTC-Regular.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangTC-Regular.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangTC-Regular.ttx
+ttx -b -m PingFangTC-Regular.otf PingFangTC-Regular.ttx
+
+ttx -t cmap PingFangTC-Semibold.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangTC-Semibold.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangTC-Semibold.ttx
+ttx -b -m PingFangTC-Semibold.otf PingFangTC-Semibold.ttx
+
+ttx -t cmap PingFangTC-Thin.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangTC-Thin.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangTC-Thin.ttx
+ttx -b -m PingFangTC-Thin.otf PingFangTC-Thin.ttx
+
+ttx -t cmap PingFangTC-Ultralight.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangTC-Ultralight.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangTC-Ultralight.ttx
+ttx -b -m PingFangTC-Ultralight.otf PingFangTC-Ultralight.ttx
+
+# PingFangHK
+ttx -t cmap PingFangHK-Light.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangHK-Light.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangHK-Light.ttx
+ttx -b -m PingFangHK-Light.otf PingFangHK-Light.ttx
+
+ttx -t cmap PingFangHK-Medium.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangHK-Medium.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangHK-Medium.ttx
+ttx -b -m PingFangHK-Medium.otf PingFangHK-Medium.ttx
+
+ttx -t cmap PingFangHK-Regular.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangHK-Regular.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangHK-Regular.ttx
+ttx -b -m PingFangHK-Regular.otf PingFangHK-Regular.ttx
+
+ttx -t cmap PingFangHK-Semibold.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangHK-Semibold.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangHK-Semibold.ttx
+ttx -b -m PingFangHK-Semibold.otf PingFangHK-Semibold.ttx
+
+ttx -t cmap PingFangHK-Thin.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangHK-Thin.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangHK-Thin.ttx
+ttx -b -m PingFangHK-Thin.otf PingFangHK-Thin.ttx
+
+ttx -t cmap PingFangHK-Ultralight.otf
+sed -i '''s/platformID="0" platEncID="3"/platformID="3" platEncID="1"/g' PingFangHK-Ultralight.ttx
+sed -i '''s/platformID="0" platEncID="4"/platformID="3" platEncID="10"/g' PingFangHK-Ultralight.ttx
+ttx -b -m PingFangHK-Ultralight.otf PingFangHK-Ultralight.ttx
